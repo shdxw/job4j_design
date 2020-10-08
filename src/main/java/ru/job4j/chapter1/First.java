f2a82c5259d3c8f604e3036794f144660ada4b9d
vlader <Vladarscb@gmail.com>
2020-10-08 21:29:57 +1000

    test commit

---
+++ src/main/java/ru/job4j/chapter1/First.java
@@ -0,0 +1,7 @@
+package ru.job4j.chapter1;
+
+public class First {
+    public static boolean bool() {
+        return true;
+    }
+}
---
+++ src/test/java/ru/job4j/chapter1/FirstTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.chapter1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FirstTest {
+
+    @Test
+    public void bool() {
+        assertTrue(First.bool());
+    }
+}
