081b3146801f3f688d47f81943a37325ab37cfc5
vlader <Vladarscb@gmail.com>
2021-03-04 17:58:55 +1000

    travis fix

--- src/main/java/ru/job4j/chapter1/UsageLog4j.java
+++ src/main/java/ru/job4j/chapter1/UsageLog4j.java
@@ -8,18 +8,18 @@ public class UsageLog4j {
     //private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
-
     public static void main(String[] args) {
-        boolean bool = false;
-        int age = 33;
-        float fl = 33.0f;
-        double db = 33.6d;
-        char ch = 'd';
-        byte bt = 120;
-        long hj = 10000000000000000L;
-        short sh = 1000;
+//        boolean bool = false;
+//        int age = 33;
+//        float fl = 33.0f;
+//        double db = 33.6d;
+//        char ch = 'd';
+//        byte bt = 120;
+//        long hj = 10000000000000000L;
+//        short sh = 1000;
+//
+//        LOG.debug("Client info : {}, {} , {}, {} , {} , {}, {}, {}",
+//                bool, age, fl, db, ch, bt, hj, sh);
 
-        LOG.debug("Client info : {}, {} , {}, {} , {} , {}, {}, {}"
-                , bool, age, fl, db, ch, bt, hj, sh);
     }
 }
